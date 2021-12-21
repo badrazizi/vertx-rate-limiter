565398fa3f12ee0e0b043994009f450c75d6cda9
Badr Azizi <badrazizi3@gmail.com>
2021-12-22 00:51:35 +0300

    update build.gradle.kts and .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     java
     kotlin("jvm") version "1.6.0"
+    id("com.github.johnrengelman.shadow").version("6.1.0")
 }
 
 group = "com.badr.vertx"
@@ -12,6 +16,20 @@ repositories {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    api("io.vertx:vertx-web:4.2.1")
-    api(kotlin("stdlib"))
+    compileOnly("io.vertx:vertx-web:4.2.1")
+    compileOnly(kotlin("stdlib"))
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "11"
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions.jvmTarget = "11"
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("fat")
+    manifest {
+        attributes["version"] = project.version
+    }
+    archiveFileName.set("Throttling.${archiveExtension.get()}")
 }
